dd67e99b69e1cccee3124c8fdaa6a5d9c83beac9
Chandra Pandey <pandey.chandra@gmail.com>
2019-06-09 10:10:51 +0100

    initial upload

---
+++ plot1.R
@@ -0,0 +1,7 @@
+pd0 <- read.table("household_power_consumption.txt", header = TRUE, sep=";",na.strings="?")
+pd0[['Date']] <- as.Date(strptime(pd0[['Date']], format='%d/%m/%Y'))
+pd0sub <- subset(pd0, Date == "2007-02-01" | Date == "2007-02-02")
+par(mfrow = c(1,1))
+png("plot1.png", width = 480, height = 480)
+hist(pd0sub$Global_active_power, col = "red", xlab = "Global Active Power(kilowatts)", ylab = "Frequency", main="Global Active Power")
+dev.off()
